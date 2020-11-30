7e140aa742bd185cd73c16f84ebfd2db7a534741
Jham <academyyyy12@gmail.com>
2020-11-30 07:27:05 +0700

    initialize commit & feat(register-user): add feature register user

---
+++ user/formatter.go
@@ -0,0 +1,23 @@
+package user
+
+//UserFormatter struct
+type UserFormatter struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Occupation string `json:"occupation"`
+	Email      string `json:"email"`
+	Token      string `json:"token"`
+}
+
+//FormatUser function for fomating data
+func FormatUser(user User, token string) UserFormatter {
+	formatter := UserFormatter{
+		ID:         user.ID,
+		Name:       user.Name,
+		Occupation: user.Occupation,
+		Email:      user.Email,
+		Token:      token,
+	}
+
+	return formatter
+}
