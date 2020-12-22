6eb9b7e310a70e273a26d1becceac16567915656
Jham <academyyyy12@gmail.com>
2020-12-22 08:39:08 +0700

    feat(campaign): add function formatter for campaigns

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,47 @@
 package campaign
+
+// CampaignFormatter type formatter
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmout     int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{}
+	campaignFormatter.ID = campaign.ID
+	campaignFormatter.UserID = campaign.UserID
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.GoalAmount = campaign.GoalAmount
+	campaignFormatter.CurrentAmout = campaign.CurrentAmount
+	campaignFormatter.ImageURL = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+// FormatCampaigns format data campaigns
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	// if len(campaigns) == 0 {
+	// 	return []CampaignFormatter{}
+	// }
+
+	//or
+	campaignsFormatter := []CampaignFormatter{}
+	//
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+
+}
