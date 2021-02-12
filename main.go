5fee2aff414d68d7113310b27c4b63e12412104a
Jham <academyyyy12@gmail.com>
2021-02-12 14:45:53 +0700

    feat(router): add cors

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -65,6 +66,7 @@ func main() {
 	transactionHandler := handler.NewTransactionHandler(transactionService)
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.Static("/images", "./images")
 
 	api := router.Group("/api/v1")
